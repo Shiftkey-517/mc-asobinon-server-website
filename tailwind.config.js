7826bbb2360010fe04e074e3555564f6bf5f64e5
Ryo Ando <sasigume@gmail.com>
2021-07-20 03:42:20 +0900

    disabled JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
  * @type {import('tailwindcss/tailwind-config').TailwindConfig }
  **/
 const tailwindConfig = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
